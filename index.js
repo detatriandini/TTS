88ec180d05caca4ea9b6ba8c13c940cfe207f17f
detatriandini <deta.triandini@gmail.com>
2022-10-19 07:48:22 +0700

    tts fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ button.addEventListener("click", function () {
     for (let i = 0; i < t1.length; i++) {
       t1[i].innerHTML = jawaban[i];
     }
-  } else if (soal === "menurun-2" && jawwban === "tadarus") {
+  } else if (soal === "menurun-2" && jawban === "tadarus") {
     alert("Jawaban anda benar");
     for (let i = 0; i < t2.length; i++) {
       t2[i].innerHTML = jawaban[i];
